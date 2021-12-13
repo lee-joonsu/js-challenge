f4b57236029f077b2946737ce66e221e54fa5fc5
junsu lee <wnstn6566@naver.com>
2021-12-13 22:22:20 +0900

    variables

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+const a = 10;
+const b = 2;
+const myName = "nico";
+
+console.log("lalalala"); // 콘솔에 log또는 print하는 역할을 함.
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
