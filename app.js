78337771c6de09eb78a3e7cf8ceb51fee617d6fe
junsu lee <wnstn6566@naver.com>
2021-12-14 20:33:31 +0900

    const and let

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const a = 10;
 const b = 2;
-const myName = "nico";
+ let myName = "nico";
 
 console.log("lalalala"); // 콘솔에 log또는 print하는 역할을 함.
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "nicolas"; //variable을 업데이트 함.(let변수의 특징) 
+
+console.log("your new name is " + myName);
