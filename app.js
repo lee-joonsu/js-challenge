56b3a4f2b2ad4e4fa93c3a65368cf9575e4409d6
junsu lee <wnstn6566@naver.com>
2021-12-16 00:46:28 +0900

    conditional

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-const calculator = {
-  plus: function (a, b) {
-    console.log("hello");
-    return a + b; // return을 하면 function은 작동을 멈추고 결과 값을 return하고 끝내버림.
-    console.log("bye bye"); // 콘솔 상에 나타나지 않음.
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
-// 변수들끼리 상호의존적임. function 안에서 console.log할 때와는 차원이 다름.
-// 원하는 변수만 콕 집어서 결과를 알 수 있음.
+if (isNaN(age) || age < 0) {
+  console.log("please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.")
+}
