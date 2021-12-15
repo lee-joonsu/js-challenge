129e60e8ac3ca81fa142a796468cd1ef7e0b25f2
junsu lee <wnstn6566@naver.com>
2021-12-15 23:16:39 +0900

    return-krAge

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonsName) {
-    console.log("hello " + otherPersonsName + " nice to meet you!");
-  },
-};
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+  ageOfForeigner + 2; // function이 function의 밖과 소통하는 방법.
+  return "hello";
+} // 작업을 하고 그 작업의 결과를 알려주는 function
 
-player.sayHello("lynn"); // sayHello 함수를 실행함.
-player.sayHello("nico");
+const krAge = calculateKrAge(age); // function을 호출하는 코드. function의 return 값이 됨.
+
+console.log(krAge);
+// function에서 console.log하는 것이 아니라 function의 return값과 같은 variable을 console.log하고 있음. 
