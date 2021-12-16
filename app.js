4e3314f1349a7c7f2e1f366f82ef690c151568e7
junsu lee <wnstn6566@naver.com>
2021-12-16 21:43:28 +0900

    HTML in Javascript

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const age = parseInt(prompt("How old are you?"));
+const title = document.getElementById("title"); //HTML요소를 자바스크립트로 가져옴.
 
-if (isNaN(age) || age < 0) {
-  console.log("please write a real positive number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever you want.")
-}
+title.innerText = "Got you!"; // 자바스크립트를 통해 요소를 변경함.
+
+console.log(title.id);
+console.log(title.className);
