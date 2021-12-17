088baa8518a679df976d6e08853bdb585eaa89f1
junsu lee <wnstn6566@naver.com>
2021-12-17 21:55:38 +0900

    CSS in Javascript part1

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
-const h1 = document.querySelector(".hello:first-child h1"); // hello라는 class 내부에 있는 h1을 가져올 수 있다는 것을 의미.
+const h1 = document.querySelector("div.hello:first-child");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color; // element의 style을 자바스크립트에서 변경함. 별로 선호하지 않음.
+  let newColor; // newColor는 아무것도 없는 비어있는 let 변수임.
+  if (currentColor === "blue") {
+    newColor = "tomato"; //할당되지 않았던 newColor변수에 "tomato"라는 값을 할당함.
+  } else {
+    newColor = "blue"; 
+  }
+  h1.style.color = newColor; //newColor 변수가 h1의 style 요소라는 것을 알려줌.
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("All GOOODS!")
-}
-
-h1.addEventListener("click", handleTitleClick); // event를 listen하는 또다른 방법
-// handleTitleClick함수를 실행시키지 않음. 유저가 title을 클릭할 경우에 자바스크립트가 실행버튼을 대신 눌러주길 바람.
-h1.addEventListener("mouseenter", handleMouseEnter); // 마우스를 title에 올릴때 이벤트 발생
-h1.addEventListener("mouseleave", handleMouseLeave); // 마우스를 title에 내릴때 이벤트 발생
-// addEventListener()를 자주 사용하는 이유는 나중에 removeEventListner를 통해서 event listener를 제거할 수 있기 때문임.
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
