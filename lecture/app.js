4484d2aef0817dba2397dc15301d7515b7004a52
junsu lee <wnstn6566@naver.com>
2021-12-21 06:39:12 +0900

    form submission

--- lecture/app.js
+++ lecture/app.js
@@ -2,8 +2,8 @@ const loginInput = document.querySelector(".login-form input");
 const loginButton = document.querySelector(".login-form button");
 
 function LoginBtnClick() {
-  console.dir(loginInput); // input에 대한 정보를 볼 수 있음.
-  console.log("hello",loginInput.value); // value는 input의 property로 input의 내용을 가져온다.
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", LoginBtnClick);
