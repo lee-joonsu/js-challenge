080fc6b8c019428eb1003f7d7a9950436ac7295e
junsu lee <wnstn6566@naver.com>
2021-12-21 22:19:52 +0900

    Saving Username

--- lecture/app.js
+++ lecture/app.js
@@ -8,6 +8,7 @@ function LoginSubmit(event) {
   event.preventDefault(); //  어떤 event의 기본 행동이든지 발생되지 않도록 막는 함수임.
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username} keep going`; // string과 변수를 하나로 합치는 방법.
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
