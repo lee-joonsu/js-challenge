ec1a0a870f992f1eb32173aa44bf4935825ea014
junsu lee <wnstn6566@naver.com>
2021-12-21 23:53:28 +0900

    super recap

--- lecture/app.js
+++ lecture/app.js
@@ -8,13 +8,12 @@ const USERNAME_KEY = "username";
 function LoginSubmit(event) {
   event.preventDefault(); //  어떤 event의 기본 행동이든지 발생되지 않도록 막는 함수임.
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username); //localStorage에 유저 이름 저장함.
-  paintGreetings(username); // paintGreetings 함수 호출(인자는 username)
-}// localStorage에 유저정보가 없다면, form이 submit되서 우리는 input으로부터 유저정보를 받고, 
-//input에서 받은 user를 가진 paintGreetings를 호출함.
+  paintGreetings(); // paintGreetings 함수 호출
+}// local storage에 뭔가를 저장하면 paintGreetings를 호출하는 순간에 그 유저정보는 이미 local storage에 저장되어 있음.
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 } // 겹치는 것 함수로 만듦. 위에 함수안에 들어있는 내용이 겹치는 내용임.
@@ -25,5 +24,5 @@ if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", LoginSubmit);
 } else {
-  paintGreetings(savedUsername); // paintGreeting 함수 호출(인자는 savedUsername)
-} // localStorage에 유저정보가 있다면 localStorage에서 받아온 user를 인자로 가지는 paintGreetings를 호출함.
+  paintGreetings(); // paintGreeting 함수 호출
+} // localStorage에 유저정보가 있다면 localStorage에서 저장된 값을 인자로 가지는 paintGreetings를 호출함.
