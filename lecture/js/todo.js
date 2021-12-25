e9297ea71742f9040aa28e9869f0c8ff97cb7019
junsu lee <wnstn6566@naver.com>
2021-12-26 00:22:30 +0900

    Loading To Dos part1

--- lecture/js/todo.js
+++ lecture/js/todo.js
@@ -2,13 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = []; // array를 만듦. newToDo가 그려질 때마다 그 텍스트를 array에 push함(넣음).
 // toDos array를 localStorage에 넣기
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 } // toDos array의 내용을 localStorage에 넣는 것임.
-
+// JSON.stringify는 자바스크립트 object나 array 또는 어떤 자바스크립트 코드건 간에 그걸 string으로 만들어 줌.
 function deleteToDo(event) {
   const li = event.target.parentElement; // 이게 우리가 삭제하고 싶은 li임.
   li.remove();
@@ -36,3 +38,15 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item) { // 자바스크립트는 지금 처리되고 있는 item 또한 그냥 제공해줌.  
+  console.log("this is the turn of", item);
+} // sayHello 함수를 길게 나타내는 방법
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  parsedToDos.forEach((item) => console.log("this is the turn of", item)); // sayHello function을 짧게 나타내는 방법. arrow function이라고 함.
+  // forEach는 각각의 item에 대해 sayHello function을 실행시킴.
+}
