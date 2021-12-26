9c2cea7c38a7227a9aee67581c0afd32dfb9d223
junsu lee <wnstn6566@naver.com>
2021-12-27 01:26:11 +0900

    졸업작품

--- app.js
+++
@@ -1,6 +0,0 @@
-const h1 = document.querySelector(".hello h1");
-
-function handleTitleClick() {
-  h1.classList.toggle("clicked"); // toggle을 사용하면 가장 간단하게(한 줄의 코드로) css를 자바스크립트에 반영할 수 있음.
-}
-h1.addEventListener("click", handleTitleClick);
