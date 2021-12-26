5e823d2750dcd40e6c27548e67a2be5a9b72d15b
junsu lee <wnstn6566@naver.com>
2021-12-26 23:57:17 +0900

    final

--- lecture/js/weather.js
+++ lecture/js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "c4ac9e215d823484996f00116cff0a6a";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }); // url을 부르는 방법. 웹문서 검사에서 network에서 확인할 수 있음.
+  // 자바스크립트에서 URL로 요청함.
 } // 위치를 얻는데 성공했을 때
 function onGeoError() {
   alert("can't find you. No weather for you");
